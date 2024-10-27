47b65dc449e4c5da3ead8c0c6ddb21fee94ffa33
Paulo Miguel <paulo.miguel.v2000@gmail.com>
2024-10-26 23:54:17 -0300

    Ajustando o vercel.json e vit.config

--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,7 @@
-import { defineConfig } from 'vite'// vite.config.js
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
-
+// https://vitejs.dev/config/
 export default defineConfig({
-  build: {
-    outDir: 'dist',
-    assetsDir: '',
-    emptyOutDir: false,
-  },
-  server: {
-    open: true,
-  },
-});
-
+  plugins: [react()],
+})
