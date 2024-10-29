90a3cf3496f5ceea87009fc9a78065b7ffda1354
Paulo Miguel <paulo.miguel.v2000@gmail.com>
2024-10-28 22:55:53 -0300

    adicionando o Analytics

--- src/components/Container/index.jsx
+++ src/components/Container/index.jsx
@@ -1,11 +1,13 @@
 import { StyledContainer } from "./container.module.jsx";
-
+import { Analytics } from "@vercel/analytics/react"
 // eslint-disable-next-line react/prop-types
 function Container({ children }) {
   return (
     <StyledContainer>
       {children}
+      <Analytics/>
     </StyledContainer>
+
   );
 }
 
